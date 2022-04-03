61bdcd256f34bf0f1b726512be8751d780d63aa7
gipsonleojr <102702296+gipsonleojr@users.noreply.github.com>
2022-04-03 18:17:21 -0400

    Add files via upload

---
+++ blastoff.js
@@ -0,0 +1,87 @@
+// The Script tags lets the browser know we are in JavaScript and not HTML
+function StartTheCountdown()
+{
+    // Purpose of this function is to start the countdown
+    // and control the countdown also
+
+    // to input variables
+    // used to display to the user.
+    var countdown = 10;
+    // used to keep track of time for the countdown
+    // 1000 = 1 second because we are using milliseconds
+    var timeout = 10000;
+    // use this set time out function for the countdown
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = "Blast Off!!!";
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+    // This process will be completed 10 times
+    setTimeout(() =>  {
+        document.getElementById("CountdownDisplay").innerHTML = countdown;
+        countdown = countdown - 1;
+    }, timeout)
+    timeout = timeout - 1000;
+
+
+}
